791f1e6df5975b2994b97d33877951d4e0bb3fd3
Halaktionova Olha <galaktionovaolga555@gmail.com>
2025-06-28 15:47:24 +0300

    Добавление баннера (#6)
    Добавила компонент Banner и его импорт и подключение на главную страницу.

--- garden-shop/src/pages/HomePage/HomePage.jsx
+++ garden-shop/src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import CategoryPanel from '../../components/CategoryPanel/CategoryPanel'
+import Banner from '../../components/Banner/Banner'
 
 const HomePage = () => {
   return (
     <div>
+        <Banner />
         <CategoryPanel item__limit={4}/>
     </div>
   )
