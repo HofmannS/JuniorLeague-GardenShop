f6dc1735e7efac5f8e525de78e928fa3b02cde10
helenkonovalova <37671989+helenkonovalova@users.noreply.github.com>
2025-06-28 11:09:58 +0200

    Добавила Header, папку images, изменила Layout (#4)

--- garden-shop/src/components/Layout/Layout.jsx
+++ garden-shop/src/components/Layout/Layout.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Footer from '../Footer/Footer';
+import Header from '../../Header/Header';
 
 const Layout = () => {
   return (
     <>
-      
+      <Header />
+      {/* <main>
+        <Outlet />
+      </main> */}
       <Footer />
     </>
   );
