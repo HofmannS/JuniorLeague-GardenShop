ade420804ee8485b3296af16a11dff235987b496
Sergej Hofmann <152594101+HofmannS@users.noreply.github.com>
2025-06-29 10:22:18 +0200

    Внес мелкие правки, чтобы все заработало (исправил адрес до Yeader, заменил margin-bottom псевдо елементом, тк марджин нельзя было закрасить) (#7)

--- garden-shop/src/components/Banner/Banner.jsx
+++ garden-shop/src/components/Banner/Banner.jsx
@@ -4,14 +4,16 @@ import "./Banner.scss"
 
 const Banner = () => {
     return (
-        <div className="banner">
-            <div className='banner__image'>
-                <img src={BannerImage} alt="Banner"
-                    className='banner__image-item' />
-            </div>
-            <div className='banner__content'>
-                <h1 className='banner__title'>Amazing Discounts on Garden Products!</h1>
-                <button className='banner__button'>Check out</button>
+        <div className="banner__wrapper">
+            <div className="banner">
+                <div className='banner__image'>
+                    <img src={BannerImage} alt="Banner"
+                        className='banner__image-item' />
+                </div>
+                <div className='banner__content'>
+                    <h1 className='banner__title'>Amazing Discounts on Garden Products!</h1>
+                    <button className='banner__button'>Check out</button>
+                </div>
             </div>
         </div>
     );
