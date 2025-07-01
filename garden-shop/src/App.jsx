d460485699a38a40ce7150778286f25dcca8d26f
admin <sergehofmann@icloud.com>
2025-07-01 10:48:18 +0200

    Доработал логику кнопки переключения тем

--- garden-shop/src/App.jsx
+++ garden-shop/src/App.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage'
 import Layout from './components/Layout/Layout'
+import CategoriesPage from './pages/CategoriesPage/CategoriesPage'
+import AllProductsPage from './pages/AllProductsPage/AllProductsPage'
+import CartPage from './pages/CartPage/CartPage'
+import FavoritesPage from './pages/FavoritesPage/FavoritesPage'
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
+import ProductDetailsPage from './pages/ProductDetails/ProductDetails'
+import ProductsByCategoryPage from './pages/ProductsByCategoryPage/ProductsByCategoryPage'
+import DiscountProductsPage from './pages/DiscountProductsPage/DiscountProductsPage'
 
 const App = () => {
   return (
@@ -9,6 +17,14 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<HomePage />} />
+          <Route path='/categories' element={<CategoriesPage />} />
+          <Route path='/products' element={<AllProductsPage />} />
+          <Route path='/cart' element={<CartPage />} />
+          <Route path='/favorites' element={<FavoritesPage />} />
+          <Route path='/notFound' element={<NotFoundPage />} />
+          <Route path='/product/:productId' element={<ProductDetailsPage />} />
+          <Route path='/category/:categoryId' element={<ProductsByCategoryPage />} />
+          <Route path='/discount' element={<DiscountProductsPage />} />
         </Route>
       </Routes>
     </div>
--- garden-shop/src/components/Header/SwitchThemeToggle/SwitchThemeToggle.jsx
+++ garden-shop/src/components/Header/SwitchThemeToggle/SwitchThemeToggle.jsx
@@ -1,16 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { AiOutlineSun } from "react-icons/ai";
 import { IoMoonOutline } from "react-icons/io5";
 import './SwitchThemeToggle.scss'
 
 const SwitchThemeToggle = () => {
-  const [isDark, setIsDark] = useState(false)
+  const [isDark, setIsDark] = useState(() => {
+    const savedTheme = localStorage.getItem('theme');
+    return savedTheme === 'dark';
+  });
+
+  useEffect(() => {
+    if (isDark) {
+      document.documentElement.classList.add('dark_theme');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.classList.remove('dark_theme');
+      localStorage.setItem('theme', 'light');
+    }
+  }, [isDark]);
 
   const switchToggle = () => {
-    setIsDark(prev => !prev)
-    document.body.classList.toggle('dark-theme', !isDark)
+    setIsDark(prev => !prev);
   };
-
   return (
     <div className="switch-theme-toggle">
     <div className={`toggle ${isDark ? 'active' : ''}`} onClick={switchToggle}>
