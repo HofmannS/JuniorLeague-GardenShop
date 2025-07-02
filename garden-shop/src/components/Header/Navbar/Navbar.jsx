777c577a7086e3b9a74a5bc54921f3faf1b8fe7e
helenkonovalova <37671989+helenkonovalova@users.noreply.github.com>
2025-07-02 21:00:51 +0200

    Feature/scrum 39 add navlink to navbar (#21)
    * Created new page Categories
    
    * Created component Breadcrumbs
    
    * Added Navlink to Navbar
    
    ---------
    
    Co-authored-by: Sergej Hofmann <152594101+HofmannS@users.noreply.github.com>

--- garden-shop/src/components/Header/Navbar/Navbar.jsx
+++ garden-shop/src/components/Header/Navbar/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import "./Navbar.scss"
-
+import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav className='navbar'>
-        <button className='navbar__discount-button'>1 day discount!</button>
-        <ul className='navbar__nav-list'>
-            <li>Main Page</li>
-            <li>Categories</li>
-            <li>All products</li>
-            <li>All sales</li>
-        </ul>
+      <button className='navbar__discount-button'>1 day discount!</button>
+      <ul className='navbar__nav-list'>
+        <li> <NavLink to="/">Main Page</NavLink></li>
+        <li> <NavLink to="/categories">Categories</NavLink></li>
+        <li> <NavLink to="/products">All products</NavLink></li>
+        <li> <NavLink to="/discount"> All sales</NavLink></li>
+      </ul>
     </nav>
   )
 }
