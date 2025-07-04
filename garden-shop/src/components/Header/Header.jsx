f685f69242b0b95ca81b10cd01b65b2839dd85ae
Pavel <48441569+PavelMaximov@users.noreply.github.com>
2025-07-04 09:57:10 +0200

    Bugfix/footer grid fix & header container fixed (#25)
    * added mixin file
    
    * fixed grid in footer & changed structure in header

--- garden-shop/src/components/Header/Header.jsx
+++ garden-shop/src/components/Header/Header.jsx
@@ -1,25 +1,32 @@
-import React from 'react'
+import React from "react";
 import { SlHandbag, SlHeart } from "react-icons/sl";
-import ThemeToggle from './SwitchThemeToggle/SwitchThemeToggle'
-import Navbar from './Navbar/Navbar'
-import './Header.scss'
-import logoIcon from '../../assets/Images/logo.svg'
-
+import ThemeToggle from "./SwitchThemeToggle/SwitchThemeToggle";
+import Navbar from "./Navbar/Navbar";
+import "./Header.scss";
+import logoIcon from "../../assets/Images/logo.svg";
 
 const Header = () => {
-    return (
-        <header className='header container'>
-            <div className='header__logo'>
-                <img src={logoIcon} alt="Логотип" />
-            </div>
-            <div className='header__icons'>
-                <button className='header__icons-favorite'><SlHeart size={38} /></button>
-                <button className='header__icons-cart'><SlHandbag size={38} /></button>
-            </div>
-            <ThemeToggle />
-            <Navbar />
-        </header>
-    )
-}
+  return (
+    <header className="header">
+      <div className="container header__inner">
+        <div className="header__left">
+          <div div className="header__logo">
+            <img src={logoIcon} alt="Логотип" />
+          </div>
+          <ThemeToggle />
+        </div>
+        <Navbar />
+        <div className="header__icons">
+          <button className="header__icons-favorite">
+            <SlHeart size={38} />
+          </button>
+          <button className="header__icons-cart">
+            <SlHandbag size={38} />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
