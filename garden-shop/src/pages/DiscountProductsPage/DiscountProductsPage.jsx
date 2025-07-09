37a361df72d5e77b09ff7a8828bae2a1c7883328
helenkonovalova <37671989+helenkonovalova@users.noreply.github.com>
2025-07-09 21:51:38 +0200

    Added breadcrumbs at all pages (#39)

--- garden-shop/src/pages/DiscountProductsPage/DiscountProductsPage.jsx
+++ garden-shop/src/pages/DiscountProductsPage/DiscountProductsPage.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ProductsPanel from '@components/ProductsPanel/ProductsPanel'
+import Breadcrumbs from '@components/Breadcrumbs/Breadcrumbs'
 
 const DiscountProductsPage = () => {
+
   return (
     <div className='discount__items container'>
+      <Breadcrumbs />
       <ProductsPanel
         title='Discounted items'
         showOnlyDiscounted={true}
         hideDiscountFilter={true}
         forceReload={true}
+        from='discount'
       />
     </div>
   )
