c28f3804371c6fb11641170730a26fcee0bd1457
admin <sergehofmann@icloud.com>
2025-07-15 14:41:42 +0200

    Added cart-form

--- garden-shop/src/components/ProductCard/ProductCard.jsx
+++ garden-shop/src/components/ProductCard/ProductCard.jsx
@@ -49,7 +49,7 @@ const [isInCart, setIsInCart] = useState(false);
     if (existingItem) {
       updatedCart = cart.filter((item) => item.id !== id);
     } else {
-      const productData = { id, image, title, price, discont_price };
+      const productData = { id, image, title, price, discont_price, quantity: 1 };
       updatedCart = [...cart, productData];
     }
   
