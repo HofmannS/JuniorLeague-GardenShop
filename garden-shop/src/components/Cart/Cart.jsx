4336a57b5dc4939c07083bcde1266c58dcf451e6
Sergej Hofmann <152594101+HofmannS@users.noreply.github.com>
2025-07-26 16:58:27 +0200

    Feature/scrum 77,78,79 cart adaptive (#66)
    * ProductCard adaptiv
    
    * Added adaptive Shopping Cart

--- garden-shop/src/components/Cart/Cart.jsx
+++ garden-shop/src/components/Cart/Cart.jsx
@@ -4,6 +4,7 @@ import CartProduct from '@components/Cart/CartProduct/CartProduct'
 import { removeFromCart, increaseQuantity, decreaseQuantity } from '@/store/features/cartSlice'
 import './Cart.scss'
 import CartForm from '@components/Cart/CartForm/CartForm'
+import { Link } from 'react-router-dom'
 
 const Cart = () => {
   const cartItems = useSelector(state => state.cart)
@@ -18,11 +19,11 @@ const Cart = () => {
     <div className="container">
       <div className="cart-panel">
         <div className="cart-panel__header">
-          <h2 className="cart-panel__header__title">Shopping cart</h2>
-          {<LineButton name={"Back to the store"} link={"/products"} />}
+          <h2 className="cart-panel__header-title">Shopping cart</h2>
+          {<LineButton className="cart-panel__header-btn" name={"Back to the store"} link={"/products"} />}
         </div>
         {cartItems.length !== 0 && (<div className="cart-panel__full">
-          <div className="cart-panel__full__list">
+          <div className="cart-panel__full-list">
             {cartItems && cartItems.map(
               (item) => (<CartProduct
                 {...item}
@@ -31,18 +32,21 @@ const Cart = () => {
                 onDecrease={() => dispatch(decreaseQuantity(item.id))}
               />))}
           </div>
-          <div className="cart-panel__full__form">
+          <div className="cart-panel__full-form">
             <CartForm cartItems={cartItems} />
           </div>
         </div>)}
         {cartItems.length === 0 && (
           <div className="cart-panel__empty">
-            <p className="cart-panel__empty__text">
+            <p className="cart-panel__empty-text">
               Looks like you have no items in your basket currently.
             </p>
-            <button className='cart-panel__empty__btn'>Continue Shopping</button>
+            <button className='cart-panel__empty-btn'>Continue Shopping</button>
           </div>
         )}
+        <div className='cart-panel__back'>
+        <Link to="/products"><button >Back to the store</button></Link>
+        </div>
       </div>
     </div>
   )
